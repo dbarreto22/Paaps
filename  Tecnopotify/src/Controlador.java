88de31df398405ec2c35dd40600e423205114c75
diebo <ddiegob2212@gmail.com>
2017-08-10 20:29:25 -0300

    lo que hice

---
+++  Tecnopotify/src/Controlador.java
@@ -0,0 +1,15 @@
+
+public class Controlador {
+    
+    private Controlador() {
+    }
+    
+    public static Controlador getInstance() {
+        return ControladorHolder.INSTANCE;
+    }
+    
+    private static class ControladorHolder {
+
+        private static final Controlador INSTANCE = new Controlador();
+    }
+}
