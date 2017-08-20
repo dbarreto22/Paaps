f7595618cea84f32f2fa2b20b8301468a5bebd09
melanydm <melanydolgay@gmail.com>
2017-08-20 14:59:19 -0300

    arreglando detalle del cliente

---  Tecnopotify/src/Cliente.java
+++  Tecnopotify/src/Cliente.java
@@ -8,16 +8,14 @@ public class Cliente extends Usuario {
     
     public Map<String, ListaParticular> listasReprParticular = new HashMap<String, ListaParticular>();
 
-    public Cliente(ListaParticular lista, String nickname, String nombre, String apellido, String mail, Date f_nac, String imagen) {
+    public Cliente(String nickname, String nombre, String apellido, String mail, Date f_nac, String imagen) {
         super(nickname, nombre, apellido, mail, f_nac, imagen);
-        //this.lista = lista;
     }
 
     public Cliente() {
         super();
     }
     
-
     public Map<String, ListaParticular> getLista() {
         return listasReprParticular;
     }
