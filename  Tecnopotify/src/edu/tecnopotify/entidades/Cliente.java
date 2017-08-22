ec41672c1eadbf94995a09e3a57161e7425e4f5f
diebo <ddiegob2212@gmail.com>
2017-08-22 19:16:54 -0300

    actualizado

---  Tecnopotify/src/edu/tecnopotify/entidades/Cliente.java
+++  Tecnopotify/src/edu/tecnopotify/entidades/Cliente.java
@@ -6,10 +6,14 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 
+@Entity
 public class Cliente extends Usuario implements Serializable{
-    
+    @OneToMany
     public Map<String, ListaParticular> listasReprParticular = new HashMap<String, ListaParticular>();
+    @OneToMany
     public Map<String, Usuario> seguidos = new HashMap<String, Usuario>();
 
 
@@ -24,9 +28,21 @@ public class Cliente extends Usuario implements Serializable{
     public Map<String, ListaParticular> getLista() {
         return listasReprParticular;
     }
-/*
-    public void setLista(ListaParticular lista) {
-        this.lista = lista;
+    
+
+    public Map<String, ListaParticular> getListasReprParticular() {
+        return listasReprParticular;
     }
-*/
+
+    public void setListasReprParticular(Map<String, ListaParticular> listasReprParticular) {
+        this.listasReprParticular = listasReprParticular;
+    }
+
+    public Map<String, Usuario> getSeguidos() {
+        return seguidos;
+    }
+
+    public void setSeguidos(Map<String, Usuario> seguidos) {
+        this.seguidos = seguidos;
+    }
 }
