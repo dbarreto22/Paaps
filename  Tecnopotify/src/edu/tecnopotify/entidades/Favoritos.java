d4f96a0940e5f86739c8821ca0de125402c27ebe
diebo <ddiegob2212@gmail.com>
2017-08-28 13:06:56 -0300

    Entity y Dt Favoritos

---  Tecnopotify/src/edu/tecnopotify/entidades/Favoritos.java
+++  Tecnopotify/src/edu/tecnopotify/entidades/Favoritos.java
@@ -1,6 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package edu.tecnopotify.entidades;
 
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
-public class Favoritos {
+
+@Entity
+public class Favoritos implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+        
+    @Column
+    @OneToMany
+    private Cliente cliente;
+    @Column
+    @ManyToMany
+    private List<Temas> listTemas;
+    @Column
+    @ManyToMany
+    private List<Album> listAlbum;
+    @Column
+    @ManyToMany
+    private List<ListaReproduccion> listRep;
+
+    public Favoritos() {
+    }
+
+    public Favoritos(Cliente cliente, List<Temas> listTemas, List<Album> listAlbum, List<ListaReproduccion> listRep) {
+        this.cliente = cliente;
+        this.listTemas = listTemas;
+        this.listAlbum = listAlbum;
+        this.listRep = listRep;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<Temas> getListTemas() {
+        return listTemas;
+    }
+
+    public void setListTemas(List<Temas> listTemas) {
+        this.listTemas = listTemas;
+    }
+
+    public List<Album> getListAlbum() {
+        return listAlbum;
+    }
+
+    public void setListAlbum(List<Album> listAlbum) {
+        this.listAlbum = listAlbum;
+    }
+
+    public List<ListaReproduccion> getListRep() {
+        return listRep;
+    }
+
+    public void setListRep(List<ListaReproduccion> listRep) {
+        this.listRep = listRep;
+    }
+
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Favoritos)) {
+            return false;
+        }
+        Favoritos other = (Favoritos) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "edu.tecnopotify.entidades.Favoritos[ id=" + id + " ]";
+    }
     
 }
